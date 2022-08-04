ce7968900633d38116611483a86a3204e27f7333
Aart Stuurman <aartstuurman@hotmail.com>
2022-08-04 17:00:42 +0200

    mean root square fitness for specialist plotting

--- visualization/plot_avg_fitness.py
+++ visualization/plot_avg_fitness.py
@@ -6,6 +6,7 @@ import pandas
 from sqlalchemy.future import select
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
+import math
 
 
 NUM_RUNS = 2
@@ -47,37 +48,66 @@ def plot_full_generalist(ax: Axes) -> None:
     mean = describe[["mean"]].values.squeeze()
     std = describe[["std"]].values.squeeze()
     plt.fill_between(eval_range, mean - std, mean + std, color="#aaaaff")
-    describe[["mean"]].plot(ax=ax, color="#0000ff")
+    describe[["mean"]].rename(columns={"mean": "Full generalist"}).plot(
+        ax=ax, color="#0000ff"
+    )
+
+
+def sqrtfitness(x):
+    return sum([math.sqrt(v) for v in x]) ** 2
 
 
 def plot_full_specialist(ax: Axes) -> None:
     db_prefix = "full_specialist"
 
-    dfs_runs = []
-    for run in range(1):
-        dfs_bodies = []
+    combined_body_fitnesses_per_run = []
+    for run in range(NUM_RUNS):
+        seperate_body_fitnesses = []
         for body_i in range(1):
             db = open_database_sqlite(
                 f"{db_prefix}_body{body_i}_run{run}"
             )  # TODO multiple runs
-            df = pandas.read_sql(
+            individuals = pandas.read_sql(
                 select(DbOpenaiESOptimizerIndividual),
                 db,
             )
-            dfs_bodies.append(df[["gen_num", "fitness"]])
-        all_bodies = pandas.concat(dfs_bodies)
-        describe_bodies = all_bodies.groupby(by="gen_num").describe()["fitness"]
-        dfs_runs.append(describe_bodies)
+            fitness_avged = (
+                individuals[["gen_num", "fitness"]]
+                .groupby(by="gen_num")
+                .agg({"fitness": "mean"})
+            )
+            seperate_body_fitnesses.append(fitness_avged)
+        seperate_body_fitnesses_df = pandas.concat(seperate_body_fitnesses)
+        combined_body_fitnesses = seperate_body_fitnesses_df.groupby(by="gen_num").agg(
+            {"fitness": sqrtfitness}
+        )
+        combined_body_fitnesses_per_run.append(combined_body_fitnesses)
 
-    all_runs = pandas.concat(dfs_runs)
+    fitnesses_per_run = pandas.concat(combined_body_fitnesses_per_run)
+    fitnesses_per_run.reset_index(inplace=True)
 
-    describe = all_runs.groupby(by="gen_num").describe()["mean"]
+    gens = pandas.unique(fitnesses_per_run[["gen_num"]].values.squeeze())
+    eval_range = [i * NUM_EVALS // len(gens) for i, _ in enumerate(gens)]
+    df_evals = pandas.DataFrame(
+        {
+            "gen_num": gens,
+            "evaluation": eval_range,
+        }
+    )
+
+    with_evals = pandas.merge(
+        fitnesses_per_run, df_evals, left_on="gen_num", right_on="gen_num", how="left"
+    )[["evaluation", "fitness"]]
+
+    describe = with_evals.groupby(by="evaluation").describe()["fitness"]
     mean = describe[["mean"]].values.squeeze()
     std = describe[["std"]].values.squeeze()
-    plt.fill_between(range(1, len(mean) + 1), mean - std, mean + std)
-    describe[["mean"]].plot(ax=ax)
+    plt.fill_between(eval_range, mean - std, mean + std, color="#ffaaaa")
+    describe[["mean"]].rename(columns={"mean": "Full specialist"}).plot(
+        ax=ax, color="#ff0000"
+    )
 
 
 plot_full_generalist(ax=ax)
-# plot_full_specialist(ax=ax)
+plot_full_specialist(ax=ax)
 plt.show()
