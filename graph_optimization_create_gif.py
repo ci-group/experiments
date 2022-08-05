2d9370439f4cd697fc6d461d7ad3ccb74e05213b
Aart Stuurman <aartstuurman@hotmail.com>
2022-08-05 15:50:22 +0200

    Fixed gif creator memory leak

--- graph_optimization_create_gif.py
+++ graph_optimization_create_gif.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 from graph_generalist_optimizer import (
     DbGraphGeneralistOptimizerGraphNodeState,
@@ -11,72 +10,82 @@ from sqlalchemy.future import select
 import os
 import PIL
 import random
+import copy
+import subprocess
 
-bodies = ["body1", "body2", "body3", "body4", "body5"]
-envs = ["flat plane"]
 
-dbname = "graph_generalist_run0"
-db = open_database_sqlite(dbname)
-process_id = 0
+def main() -> None:
+    bodies = ["body1", "body2", "body3", "body4", "body5"]
+    envs = ["flat plane"]
 
-df = pandas.read_sql(
-    select(DbGenotype, DbGraphGeneralistOptimizerGraphNodeState).filter(
-        (DbGraphGeneralistOptimizerGraphNodeState.process_id == process_id)
-        & (DbGraphGeneralistOptimizerGraphNodeState.genotype_id == DbGenotype.id)
-    ),
-    db,
-)
-min_id = df.genotype_id.min()
-max_id = df.genotype_id.max()
+    dbname = "graph_generalist_run0"
+    figsdir = f"./{dbname}_figs"
 
-colormap = {id + min_id: id for id in range(max_id - min_id + 1)}
-shuffledvalues = list(colormap.values())
-random.shuffle(shuffledvalues)
-colormap = dict(zip(colormap, shuffledvalues))
+    db = open_database_sqlite(dbname)
+    process_id = 0
 
-figsdir = f"./{dbname}_figs"
-os.makedirs(figsdir)
+    df = pandas.read_sql(
+        select(DbGenotype, DbGraphGeneralistOptimizerGraphNodeState).filter(
+            (DbGraphGeneralistOptimizerGraphNodeState.process_id == process_id)
+            & (DbGraphGeneralistOptimizerGraphNodeState.genotype_id == DbGenotype.id)
+        ),
+        db,
+    )
+    min_id = df.genotype_id.min()
+    max_id = df.genotype_id.max()
 
-for gen_i in range(len(df.gen_num.value_counts())):
-    gen = df[df.gen_num == gen_i]
+    colormap = {id + min_id: id for id in range(max_id - min_id + 1)}
+    shuffledvalues = list(colormap.values())
+    random.shuffle(shuffledvalues)
+    colormap = dict(zip(colormap, shuffledvalues))
 
-    ids = np.array([gen.genotype_id])
-    reassigned_colors = ids.copy()
+    os.makedirs(figsdir)
 
-    for i in range(len(ids)):
-        for j in range(len(ids[i])):
-            reassigned_colors[i][j] = colormap[ids[i][j]]
+    for gen_i in range(len(df.gen_num.value_counts())):
+        gen = df[df.gen_num == gen_i]
 
-    fig, ax = plt.subplots()
-    im = ax.imshow(reassigned_colors, vmin=min_id, vmax=max_id)
+        ids = np.array([gen.genotype_id])
+        reassigned_colors = ids.copy()
 
-    ax.set_xticks(np.arange(len(bodies)), labels=bodies)
-    ax.set_yticks(np.arange(len(envs)), labels=envs)
+        for i in range(len(ids)):
+            for j in range(len(ids[i])):
+                reassigned_colors[i][j] = colormap[ids[i][j]]
 
-    # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+        fig, ax = plt.subplots()
+        ax.imshow(reassigned_colors, vmin=min_id, vmax=max_id)
 
-    for i in range(len(envs)):
-        for j in range(len(bodies)):
-            text = ax.text(j, i, ids[i, j], ha="center", va="center", color="w")
+        ax.set_xticks(np.arange(len(bodies)), labels=bodies)
+        ax.set_yticks(np.arange(len(envs)), labels=envs)
 
-    ax.set_title(f"Graph optimizer generation {gen_i}")
-    fig.tight_layout()
-    plt.savefig(os.path.join(figsdir, f"gen_{gen_i}.png"))
+        # Rotate the tick labels and set their alignment.
+        # plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
 
-frames = [
-    PIL.Image.open(image)
-    for image in [
-        os.path.join(figsdir, f"gen_{gen_i}.png")
-        for gen_i in range(len(df.gen_num.value_counts()))
-    ]
-]
-frame_one = frames[0]
-frame_one.save(
-    os.path.join(figsdir, "evolution.gif"),
-    format="GIF",
-    append_images=frames,
-    save_all=True,
-    duration=500,
-    loop=0,
-)
+        for i in range(len(envs)):
+            for j in range(len(bodies)):
+                text = ax.text(j, i, ids[i, j], ha="center", va="center", color="w")
+
+        ax.set_title(f"Graph optimizer\nGenotype for each node\nGeneration {gen_i:05d}")
+        fig.tight_layout()
+        fig.savefig(os.path.join(figsdir, f"gen_{gen_i:05d}.png"))
+        plt.close(fig)
+
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-framerate",
+            "20",
+            "-pattern_type",
+            "glob",
+            "-i",
+            f"{figsdir}/*.png",
+            "-c:v",
+            "libx264",
+            "-pix_fmt",
+            "yuv420p",
+            f"{figsdir}/evolution.mp4",
+        ]
+    )
+
+
+if __name__ == "__main__":
+    main()
