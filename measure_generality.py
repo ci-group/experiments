53a7d996bc95af98f23a089b2ff9b4cc5fcc4243
Aart Stuurman <aartstuurman@hotmail.com>
2022-08-17 14:16:39 +0200

    Loading of generalist, specialist

--- measure_generality.py
+++ measure_generality.py
@@ -1,6 +1,6 @@
 import numpy.typing as npt
 import numpy as np
-from typing import List, Dict
+from typing import List, Dict, Tuple
 from bodies import make_bodies, make_cpg_network_structure
 from evaluator import Evaluator, Setting, Environment
 from sqlalchemy.ext.asyncio import AsyncEngine
@@ -8,23 +8,88 @@ from sqlalchemy.ext.asyncio.session import AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.future import select
 import sqlalchemy
-from revolve2.core.database import open_async_database_sqlite
+from revolve2.core.database import open_async_database_sqlite, open_database_sqlite
 import os
+from revolve2.core.optimization.ea.openai_es import DbOpenaiESOptimizerIndividual
+import pandas
+from revolve2.core.database.serializers import DbNdarray1xnItem
 
 EVAL_TIME = 30
 SIMULATION_TIME = 30
 SAMPLING_FREQUENCY = 5
 CONTROL_FREQUENCY = 60
+NUM_RUNS = 2
+NUM_BODIES = 5
 
 
-def get_specialist_brains() -> List[
-    List[npt.NDArray[np.float_]]
-]:  # runs -> bodies -> brain
-    return []  # TODO
+async def get_specialist_brains() -> List[
+    Tuple[str, List[List[npt.NDArray[np.float_]]]]
+]:  # [params, runs -> bodies -> brain]
+    res = []
+    for params_str in ["s0.5l0.1"]:  # , "s0.05l0.01"]:
+        res_run = []
+        for run_i in range(NUM_RUNS):
+            res_body = []
+            for body_i in range(NUM_BODIES):
+                db = open_database_sqlite(
+                    f"full_specialist_{params_str}_body{body_i}_run{run_i}"
+                )
+                individuals = pandas.read_sql(
+                    select(DbOpenaiESOptimizerIndividual, DbNdarray1xnItem).filter(
+                        DbOpenaiESOptimizerIndividual.individual
+                        == DbNdarray1xnItem.nparray1xn_id
+                    ),
+                    db,
+                )
+                individuals["fitness"] = individuals["fitness"] / EVAL_TIME
+                individuals = individuals[
+                    individuals.gen_num == individuals.gen_num.max()
+                ]
+                all_params = [
+                    np.array(
+                        individuals[individuals.gen_index == gen_i]
+                        .sort_values(by=["array_index"])
+                        .value
+                    )
+                    for gen_i in range(individuals.gen_index.max())
+                ]
+                avg_params = sum(all_params) / len(all_params)
+                res_body.append(avg_params)
+            res_run.append(res_body)
+        res.append((params_str, res_run))
+    return []
 
 
-def get_generalist_brains() -> List[npt.NDArray[np.float_]]:  # runs -> brain
-    return []  # TODO
+def get_generalist_brains() -> List[
+    Tuple[str, List[npt.NDArray[np.float_]]]
+]:  # [params, runs -> brain]
+    res = []
+    for params_str in ["s0.5l0.1", "s0.05l0.01"]:
+        res_run = []
+        for run_i in range(NUM_RUNS):
+            db = open_database_sqlite(f"full_generalist_{params_str}_run{run_i}")
+            individuals = pandas.read_sql(
+                select(DbOpenaiESOptimizerIndividual, DbNdarray1xnItem).filter(
+                    DbOpenaiESOptimizerIndividual.individual
+                    == DbNdarray1xnItem.nparray1xn_id
+                ),
+                db,
+            )
+            individuals["fitness"] = individuals["fitness"] / EVAL_TIME
+            individuals = individuals[individuals.gen_num == individuals.gen_num.max()]
+            all_params = [
+                np.array(
+                    individuals[individuals.gen_index == gen_i]
+                    .sort_values(by=["array_index"])
+                    .value
+                )
+                for gen_i in range(individuals.gen_index.max())
+            ]
+            avg_params = sum(all_params) / len(all_params)
+            res_run.append(avg_params)
+        res.append((params_str, res_run))
+
+    return []
 
 
 def get_graph_brains() -> List[List[npt.NDArray[np.float_]]]:  # runs -> bodies -> brain
@@ -45,16 +110,18 @@ async def main() -> None:
 
     all_brains = []
     all_brain_names_suffix = []
-    for run_i, bodies in enumerate(get_specialist_brains()):
-        for body_i, brain in enumerate(bodies):
-            all_brains.append(brain)
-            all_brain_names_suffix.append(
-                f"specialist_run{run_i}_optimizedforbody{body_i}"
-            )
+    for params, runs in await get_specialist_brains():
+        for run_i, bodies in enumerate(runs):
+            for body_i, brain in enumerate(bodies):
+                all_brains.append(brain)
+                all_brain_names_suffix.append(
+                    f"specialist_{params}_run{run_i}_optimizedforbody{body_i}"
+                )
 
-    for run_i, brain in enumerate(get_generalist_brains()):
-        all_brains.append(brain)
-        all_brain_names_suffix.append(f"generalist_run{run_i}")
+    for params, runs in get_generalist_brains():
+        for run_i, brain in enumerate(runs):
+            all_brains.append(brain)
+            all_brain_names_suffix.append(f"generalist_{params}_run{run_i}")
 
     for run_i, bodies in enumerate(get_graph_brains()):
         for body_i, brain in enumerate(bodies):
