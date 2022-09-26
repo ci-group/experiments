4c0eb97ff9be9cd26b41cedcd8c9417610640323
Aart Stuurman <aartstuurman@hotmail.com>
2022-09-26 17:30:15 +0200

    heatmap

--- plot_generality.py
+++ plot_generality.py
@@ -3,6 +3,7 @@ from revolve2.core.database import open_database_sqlite
 import pandas
 from sqlalchemy.future import select
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 DB_NAME = "dbs_isaac/final_fitnesses"
 NUM_BODIES = 5
@@ -16,6 +17,14 @@ async def main() -> None:
         db,
     )
 
+    # mean = df[["brain_name", "fitness"]].mean()
+    # std = df[["brain_name", "fitness"]].std()
+
+    # df[["brain_name", "fitness"]].boxplot(by="brain_name", rot=10)
+    # plt.suptitle(None)
+    # plt.title("Aggregate of fitnesses in all environments")
+    # plt.show()
+
     avgedruns = (
         df[["body", "brain_name", "fitness"]]
         .groupby(["body", "brain_name"])
@@ -23,18 +32,34 @@ async def main() -> None:
         .reset_index()
     )
 
-    grouped = avgedruns[["brain_name", "fitness"]].groupby(
-        ["brain_name"], as_index=False
+    g = sns.heatmap(
+        avgedruns.pivot(index="body", columns="brain_name", values="fitness"),
+        annot=True,
+        fmt=".3g",
+        cmap="viridis",
     )
-
-    mean = grouped.mean()
-    std = grouped.std()
-
-    avgedruns[["brain_name", "fitness"]].boxplot(by="brain_name", rot=10)
-    plt.suptitle(None)
-    plt.title("Aggregate of fitnesses in all environments")
+    g.set_xticklabels(g.get_xticklabels(), rotation=10)
     plt.show()
-    # plt.savefig("generalist_measure.png")
+
+    # avgedruns = (
+    #     df[["body", "brain_name", "fitness"]]
+    #     .groupby(["body", "brain_name"])
+    #     .mean()
+    #     .reset_index()
+    # )
+
+    # grouped = avgedruns[["brain_name", "fitness"]].groupby(
+    #     ["brain_name"], as_index=False
+    # )
+
+    # mean = grouped.mean()
+    # std = grouped.std()
+
+    # avgedruns[["brain_name", "fitness"]].boxplot(by="brain_name", rot=10)
+    # plt.suptitle(None)
+    # plt.title("Aggregate of fitnesses in all environments")
+    # plt.show()
+    # # plt.savefig("generalist_measure.png")
 
 
 if __name__ == "__main__":
