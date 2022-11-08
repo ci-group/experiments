042d6eb160ed97d2c462c31e68577d2ddedc323a
Aart Stuurman <aartstuurman@hotmail.com>
2022-11-08 13:34:41 +0100

    plot graph

--- v2/plot_avg_fitness.py
+++ v2/plot_avg_fitness.py
@@ -6,8 +6,15 @@ import pandas
 from sqlalchemy.future import select
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
-from de_multi_body_optimizer import Population, Measures, ProgramState
-from experiment_settings import NUM_RUNS, NUM_EVALUATIONS, SIMULATION_TIME, DE_PARAMS
+import de_multi_body_optimizer
+import graph_optimizer
+from experiment_settings import (
+    NUM_RUNS,
+    NUM_EVALUATIONS,
+    SIMULATION_TIME,
+    DE_PARAMS,
+    GRAPH_PARAMS,
+)
 from bodies import make_bodies
 from typing import List
 import math
@@ -31,14 +38,22 @@ def plot_full_generalist(ax: Axes) -> None:
             )
             df = pandas.read_sql(
                 select(
-                    ProgramState.table, Population.item_table, Measures.table
+                    de_multi_body_optimizer.ProgramState.table,
+                    de_multi_body_optimizer.Population.item_table,
+                    de_multi_body_optimizer.Measures.table,
                 ).filter(
-                    (ProgramState.table.population == Population.item_table.list_id)
-                    & (Population.item_table.measures == Measures.table.id)
+                    (
+                        de_multi_body_optimizer.ProgramState.table.population
+                        == de_multi_body_optimizer.Population.item_table.list_id
+                    )
+                    & (
+                        de_multi_body_optimizer.Population.item_table.measures
+                        == de_multi_body_optimizer.Measures.table.id
+                    )
                 ),
                 db,
             )
-            df["fitness"] = df["fitness"] / SIMULATION_TIME * 60
+            df["fitness"] = df["fitness"] / SIMULATION_TIME * 10
             dfs_per_run.append(df[["generation_index", "fitness"]])
 
         max_per_run = []
@@ -96,14 +111,22 @@ def plot_full_specialist(ax: Axes) -> None:
                 )
                 df = pandas.read_sql(
                     select(
-                        ProgramState.table, Population.item_table, Measures.table
+                        de_multi_body_optimizer.ProgramState.table,
+                        de_multi_body_optimizer.Population.item_table,
+                        de_multi_body_optimizer.Measures.table,
                     ).filter(
-                        (ProgramState.table.population == Population.item_table.list_id)
-                        & (Population.item_table.measures == Measures.table.id)
+                        (
+                            de_multi_body_optimizer.ProgramState.table.population
+                            == de_multi_body_optimizer.Population.item_table.list_id
+                        )
+                        & (
+                            de_multi_body_optimizer.Population.item_table.measures
+                            == de_multi_body_optimizer.Measures.table.id
+                        )
                     ),
                     db,
                 )
-                df["fitness"] = df["fitness"] / SIMULATION_TIME * 60
+                df["fitness"] = df["fitness"] / SIMULATION_TIME * 10
                 dfs_per_body.append(df[["generation_index", "fitness"]])
             dfs_per_run_per_body.append(dfs_per_body)
 
@@ -154,53 +177,78 @@ def plot_full_specialist(ax: Axes) -> None:
         ).plot(ax=ax, color=colorb)
 
 
-# def plot_graph(ax: Axes) -> None:
-#     db_prefix = "dbs/graph_generalist"
+def plot_graph(ax: Axes) -> None:
+    db_prefix = "dbs/graph"
 
-#     combined_body_fitnesses_per_run = []
-#     for run in range(NUM_RUNS):
-#         db = open_database_sqlite(f"{db_prefix}_run{run}")
-#         seperate_body_fitnesses = pandas.read_sql(
-#             select(DbGraphGeneralistOptimizerGraphNodeState),
-#             db,
-#         )[["gen_num", "graph_index", "fitness"]]
-#         seperate_body_fitnesses["fitness"] = (
-#             seperate_body_fitnesses["fitness"] / SIMULATION_TIME
-#         )
-#         combined_body_fitnesses = seperate_body_fitnesses.groupby(by=["gen_num"]).agg(
-#             {"fitness": sqrtfitness}
-#         )
-#         combined_body_fitnesses_per_run.append(combined_body_fitnesses)
+    for (standard_deviation), (
+        colora,
+        colorb,
+    ) in zip(GRAPH_PARAMS, [("#aaaa00", "#dddd00")]):
+        fitnesses_per_run: List[pandas.DataFrame] = []
+        for run in range(NUM_RUNS):
+            # db = open_database_sqlite(f"{db_prefix}_s{standard_deviation}_run{run}")
+            db = open_database_sqlite(f"dbg_graph")
+            df = pandas.read_sql(
+                select(
+                    graph_optimizer.ProgramState.table,
+                    graph_optimizer.Population.item_table,
+                    graph_optimizer.Measures.table,
+                ).filter(
+                    (
+                        graph_optimizer.ProgramState.table.population
+                        == graph_optimizer.Population.item_table.list_id
+                    )
+                    & (
+                        graph_optimizer.Population.item_table.measures
+                        == graph_optimizer.Measures.table.id
+                    )
+                ),
+                db,
+            )[["generation_index", "fitness"]]
+            df["fitness"] = df["fitness"] / SIMULATION_TIME * 10
 
-#     fitnesses_per_run = pandas.concat(combined_body_fitnesses_per_run)
-#     fitnesses_per_run.reset_index(inplace=True)
+            combined_fitnesses_per_gen = (
+                df.groupby(by=["generation_index"])
+                .agg({"fitness": sqrtfitness})
+                .reset_index()
+            )
 
-#     gens = pandas.unique(fitnesses_per_run[["gen_num"]].values.squeeze())
-#     eval_range = [i * NUM_EVALUATIONS // len(gens) for i, _ in enumerate(gens)]
-#     df_evals = pandas.DataFrame(
-#         {
-#             "gen_num": gens,
-#             "evaluation": eval_range,
-#         }
-#     )
+            fitnesses_per_run.append(combined_fitnesses_per_gen)
 
-#     with_evals = pandas.merge(
-#         fitnesses_per_run, df_evals, left_on="gen_num", right_on="gen_num", how="left"
-#     )[["evaluation", "fitness"]]
+        fitnesses = pandas.concat(fitnesses_per_run)
 
-#     describe = with_evals.groupby(by="evaluation").describe()["fitness"]
-#     mean = describe[["mean"]].values.squeeze()
-#     std = describe[["std"]].values.squeeze()
-#     plt.fill_between(eval_range, mean - std, mean + std, color="#dddd00")
-#     describe[["mean"]].rename(columns={"mean": "Graph optimization"}).plot(
-#         ax=ax, color="#aaaa00"
-#     )
+        gens = pandas.unique(fitnesses[["generation_index"]].values.squeeze())
+        eval_range = [
+            (i + 1) * NUM_EVALUATIONS // len(gens) for i, _ in enumerate(gens)
+        ]
+        df_evals = pandas.DataFrame(
+            {
+                "generation_index": gens,
+                "evaluation": eval_range,
+            }
+        )
+
+        with_evals = pandas.merge(
+            fitnesses,
+            df_evals,
+            left_on="generation_index",
+            right_on="generation_index",
+            how="left",
+        )[["evaluation", "fitness"]]
+
+        describe = with_evals.groupby(by="evaluation").describe()["fitness"]
+        mean = describe[["mean"]].values.squeeze()
+        std = describe[["std"]].values.squeeze()
+        plt.fill_between(eval_range, mean - std, mean + std, color=colora)
+        describe[["mean"]].rename(
+            columns={"mean": f"Graph optimization (s{standard_deviation})"}
+        ).plot(ax=ax, color=colorb)
 
 
 plot_full_generalist(ax=ax)
 plot_full_specialist(ax=ax)
-# plot_graph(ax=ax)
+plot_graph(ax=ax)
 ax.set_xlabel("Number of evaluations")
-ax.set_ylabel("Fitness (approx. m/min)")
+ax.set_ylabel("Fitness (approx. cm/s)")
 plt.title("Graph optimization and baseline performance")
 plt.show()
