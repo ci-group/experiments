9fe3f3886d2d18e88d85b5a3abaa1c72d5102932
Aart Stuurman <aartstuurman@hotmail.com>
2022-11-17 15:12:11 +0100

    terrain generation. bowl and rugged

--- v2/terrain_generator.py
+++ v2/terrain_generator.py
@@ -1,46 +1,39 @@
 from revolve2.core.physics import Terrain
 from pyrr import Vector3, Quaternion
 from revolve2.core.physics.running import geometry
-from typing import Tuple
+from typing import Tuple, List
 from noise import pnoise2
+import math
 
 
 def terrain_generator(
     size: Tuple[float, float],
-    max_height: float,
-    density: float = 1.0,
+    ruggedness: float,
+    bowlness: float,
     granularity_multiplier: float = 1.0,
 ) -> Terrain:
-    import matplotlib.pyplot as plt
-
-    SIZE = 5.0
-    NUM_EDGES = 400
-    OCTAVE = 10
-    SCALE = 20
+    NUM_EDGES = 100
 
     num_edges = (
-        int(granularity_multiplier * NUM_EDGES * size[0] / SIZE),
-        int(granularity_multiplier * NUM_EDGES * size[1] / SIZE),
+        int(NUM_EDGES * size[0] * granularity_multiplier),
+        int(NUM_EDGES * size[1] * granularity_multiplier),
     )
 
-    if max_height == 0.0:
-        heights = [[0.0 for y in range(num_edges[1])] for x in range(num_edges[0])]
-        max_height = 1.0
-    else:
-        heights = [
-            [
-                pnoise2(
-                    x / num_edges[0] * SCALE / SIZE * size[0] * density,
-                    y / num_edges[1] * SCALE / SIZE * size[1] * density,
-                    OCTAVE,
-                )
-                for y in range(num_edges[1])
-            ]
-            for x in range(num_edges[0])
-        ]
+    rugged = rugged_heightmap(
+        size=size,
+        num_edges=num_edges,
+        density=1.5,
+    )
+    bowl = bowl_heighmap(num_edges=num_edges)
 
-    plt.imshow(heights, cmap="gray")
-    plt.show()
+    heightmap = [
+        [
+            (yrugged * ruggedness + ybowl * bowlness) / (ruggedness + bowlness)
+            for yrugged, ybowl in zip(xrugged, xbowl)
+        ]
+        for xrugged, xbowl in zip(rugged, bowl)
+    ]
+    max_height = ruggedness + bowlness
 
     return Terrain(
         static_geometry=[
@@ -48,8 +41,48 @@ def terrain_generator(
                 position=Vector3(),
                 orientation=Quaternion(),
                 size=Vector3([size[0], size[1], max_height]),
-                base_thickness=max_height + 0.1,
-                heights=heights,
+                base_thickness=0.1 + ruggedness,
+                heights=heightmap,
             )
         ]
     )
+
+
+def rugged_heightmap(
+    size: Tuple[float, float],
+    num_edges: Tuple[int, int],
+    density: float = 1.0,
+) -> List[List[float]]:
+    # TODO the maximum height is not really 1.0.
+    OCTAVE = 10
+    C1 = 4.0  # arbitrary constant to get nice noise
+
+    return [
+        [
+            pnoise2(
+                x / num_edges[0] * C1 * size[0] * density,
+                y / num_edges[1] * C1 * size[1] * density,
+                OCTAVE,
+            )
+            for y in range(num_edges[1])
+        ]
+        for x in range(num_edges[0])
+    ]
+
+
+def bowl_heighmap(
+    num_edges: Tuple[int, int],
+) -> List[List[float]]:
+    return [
+        [
+            (x / num_edges[0] * 2.0 - 1.0) ** 2 + (y / num_edges[1] * 2.0 - 1.0) ** 2
+            if math.sqrt(
+                (x / num_edges[0] * 2.0 - 1.0) ** 2
+                + (y / num_edges[1] * 2.0 - 1.0) ** 2
+            )
+            <= 1.0
+            else 0.0
+            for y in range(num_edges[1])
+        ]
+        for x in range(num_edges[0])
+    ]
