31c98b08fdb9397cda271382f0b6b3e212dc91dd
Aart Stuurman <aartstuurman@hotmail.com>
2022-11-22 11:35:50 +0100

    std 0.05 for graph alg

--- v2/experiment_settings.py
+++ v2/experiment_settings.py
@@ -15,7 +15,7 @@ DE_PARAMS = [
 ]  # population_size, crossover_probability, differential_weight
 
 # Graph settings
-GRAPH_PARAMS = (0.2,)  # standard deviation
+GRAPH_PARAMS = (0.05,)  # standard deviation
 
 # Terrain parameters
 TERRAIN_SIZE = (5.0, 5.0)
