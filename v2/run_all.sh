69f774bda9a0de1aa2ea4e6edd466dd5ffed05d2
Aart Stuurman <aartstuurman@hotmail.com>
2022-11-22 11:50:53 +0100

    parallel 10

--- v2/run_all.sh
+++ v2/run_all.sh
@@ -1,3 +1,3 @@
 python $(dirname "$0")/run_full_generalist.py
 python $(dirname "$0")/run_full_specialist.py
-seq 0 $(cd $(dirname "$0"); python -c"from experiment_settings import NUM_RUNS;print(NUM_RUNS-1)") | parallel -j 60 --workdir $PWD python $(dirname "$0")/run_graph_generalist.py {}
+seq 0 $(cd $(dirname "$0"); python -c"from experiment_settings import NUM_RUNS;print(NUM_RUNS-1)") | parallel -j 10 --workdir $PWD python $(dirname "$0")/run_graph_generalist.py {}
