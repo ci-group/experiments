5c4d48ade71b6bc8d564e37c2dd5edcf03e05b12
Aart Stuurman <aartstuurman@hotmail.com>
2022-12-16 15:24:28 +0100

    Add experiment settings for graph

--- v2/experiment_settings.py
+++ v2/experiment_settings.py
@@ -5,26 +5,115 @@ SIMULATION_TIME = 30
 SAMPLING_FREQUENCY = 5
 CONTROL_FREQUENCY = 60
 
-NUM_EVALUATIONS = 500000
+NUM_EVALUATIONS = 300000
 
 # DE settings
 DE_PARAMS = [
-    (100, 0.9, 0.8),
-    (100, 0.9, 0.2),
-    (10, 0.9, 0.2),
+    (50, 0.9, 0.2),
 ]  # population_size, crossover_probability, differential_weight
 
 # Graph settings
+GRAPH_STD = 0.05
+GRAPH_PMIG = 0.5
+GRAPH_ALPHA1 = 1.0
+GRAPH_ALPHA2 = 8.0
 GRAPH_PARAMS = [
     (
-        0.05,
+        GRAPH_STD,
+        GRAPH_PMIG,
+        GRAPH_ALPHA1,
+        GRAPH_ALPHA2,
         0.5,
-        1.0,
+        0.5,
+    ),
+    (
+        GRAPH_STD,
+        GRAPH_PMIG,
+        GRAPH_ALPHA1,
+        GRAPH_ALPHA2,
+        0.0,
+        0.0,
+    ),
+    (
+        GRAPH_STD,
+        GRAPH_PMIG,
+        GRAPH_ALPHA1,
+        GRAPH_ALPHA2,
+        0.5,
+        0.0,
+    ),
+    (
+        GRAPH_STD,
+        GRAPH_PMIG,
+        GRAPH_ALPHA1,
+        GRAPH_ALPHA2,
         1.0,
         0.0,
+    ),
+    (
+        GRAPH_STD,
+        GRAPH_PMIG,
+        GRAPH_ALPHA1,
+        GRAPH_ALPHA2,
+        0.0,
+        0.5,
+    ),
+    (
+        GRAPH_STD,
+        GRAPH_PMIG,
+        GRAPH_ALPHA1,
+        GRAPH_ALPHA2,
+        1.0,
+        0.5,
+    ),
+    (
+        GRAPH_STD,
+        GRAPH_PMIG,
+        GRAPH_ALPHA1,
+        GRAPH_ALPHA2,
+        0.0,
+        1.0,
+    ),
+    (
+        GRAPH_STD,
+        GRAPH_PMIG,
+        GRAPH_ALPHA1,
+        GRAPH_ALPHA2,
+        0.5,
+        1.0,
+    ),
+    (
+        GRAPH_STD,
+        GRAPH_PMIG,
+        GRAPH_ALPHA1,
+        GRAPH_ALPHA2,
+        1.0,
+        1.0,
+    ),
+    (
+        GRAPH_STD,
+        GRAPH_PMIG,
+        GRAPH_ALPHA1,
+        GRAPH_ALPHA2,
+        0.0,
         float("inf"),
-    ),  # innovate and migrate if and only if new fitness is better
-    (0.05, 0.0, 1.0, 1.0, 0.0, float("inf")),
+    ),
+    (
+        GRAPH_STD,
+        GRAPH_PMIG,
+        GRAPH_ALPHA1,
+        GRAPH_ALPHA2,
+        0.5,
+        float("inf"),
+    ),
+    (
+        GRAPH_STD,
+        GRAPH_PMIG,
+        GRAPH_ALPHA1,
+        GRAPH_ALPHA2,
+        1.0,
+        float("inf"),
+    ),
 ]  # standard deviation, migration_probability, alpha1, alpha2, theta1, theta2
 
 # Terrain parameters
