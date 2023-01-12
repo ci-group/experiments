8b484401bfe6bc861338294ce8eb1f8576f715f3
Aart Stuurman <astuurman@hotmail.com>
2023-01-12 15:08:25 +0100

    partitioning

---
+++ v2/partition.py
@@ -0,0 +1,89 @@
+from graph import Graph, Node
+from environment import Environment
+from typing import List
+import math
+import numpy as np
+from make_graph import make_graph
+from collections import deque
+import random
+
+
+def dist(env1: Environment, env2: Environment) -> float:
+    return math.sqrt(
+        (env1.name.body_num - env2.name.body_num) ** 2
+        + (env1.name.ruggedness_num - env2.name.ruggedness_num) ** 2
+        + (env1.name.bowlness_num - env2.name.bowlness_num) ** 2
+    )
+
+
+def find_start_points(
+    graph: Graph, envs: List[Environment], num_points: int, rng: random.Random
+) -> List[Node]:
+    points = [
+        graph.nodes[rng.randint(0, len(graph.nodes) - 1)] for _ in range(num_points)
+    ]
+    for z in range(500):
+        print(z)
+        for i, point in enumerate(points):
+            neighbor_scores = [
+                min(
+                    [
+                        dist(envs[neighbor.index], envs[other_point.index])
+                        for j, other_point in enumerate(points)
+                        if i != j
+                    ]
+                )
+                for neighbor in point.neighbours
+            ]
+            best = np.argmax(neighbor_scores)
+            points[i] = point.neighbours[best]
+    return points
+
+
+def partition(
+    graph: Graph, envs: List[Environment], num_partitions: int
+) -> List[Graph]:
+    rng = random.Random()
+    rng.seed(100)
+
+    subgraphs: List[List[Node]]
+
+    if num_partitions == 125:
+        subgraphs = [[graph.nodes[i]] for i in range(num_partitions)]
+    elif num_partitions == 1:
+        return [graph]
+    else:
+        partition_centers = find_start_points(graph, envs, num_partitions, rng)
+
+        opens: List[deque[Node]] = [deque() for _ in range(num_partitions)]
+        taken: List[Node] = []
+        subgraphs = [[] for _ in range(num_partitions)]
+
+        for i in range(num_partitions):
+            taken.append(partition_centers[i])
+            subgraphs[i].append(partition_centers[i])
+            for neighbor in partition_centers[i].neighbours:
+                opens[i].append(neighbor)
+
+        while len(taken) < len(graph.nodes):
+            for i in range(num_partitions):
+                item = None
+                while len(opens[i]) != 0:
+                    get = opens[i].pop()
+                    if get not in taken:
+                        item = get
+                        break
+
+                if item is not None:
+                    subgraphs[i].append(item)
+                    taken.append(item)
+                    for neighbor in item.neighbours:
+                        if neighbor not in taken:
+                            opens[i].append(neighbor)
+
+    for subgraph in subgraphs:
+        for node in subgraph:
+            node.neighbor = [
+                neighbor for neighbor in node.neighbours if neighbor in subgraph
+            ]
+    return [Graph(subgraph) for subgraph in subgraphs]
