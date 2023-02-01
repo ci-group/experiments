2004e2429d78876a95db160a523dd4891f03a0b7
Aart Stuurman <astuurman@hotmail.com>
2023-02-01 14:54:53 +0100

    fix cmaes db name

--- v2/experiments.py
+++ v2/experiments.py
@@ -41,7 +41,7 @@ def cmaes_database_name(
     partition_size: int,
     partition_num: int,
 ) -> str:
-    return f"de_psize{partition_size}_pnum{partition_num}_run{run}"
+    return f"cmaes_psize{partition_size}_pnum{partition_num}_run{run}"
 
 
 async def run_graph(
