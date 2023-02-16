363d5223f6fc246c8156f08a3e6dcd89673773ce
Aart Stuurman <astuurman@hotmail.com>
2023-02-16 10:37:42 +0100

    disable cmaes experiment partition size 1

--- v2/experiment_settings.py
+++ v2/experiment_settings.py
@@ -17,7 +17,7 @@ DE_PARAMS = [
 ]  # population_size, crossover_probability, differential_weight, partition_size
 
 CMAES_PARAMS = [
-    (0.5, 1),
+    # (0.5, 1),
     (0.5, 125),
     (0.5, 5),
     (0.5, 25),
